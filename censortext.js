5ae6363b63182d72b38f8d819eecdababfd38083
敬庭超-东走西走-技术中心-生活服务项目组 <jingtingchao@long.local>
2018-07-17 09:32:43 +0800

    first commit

---
+++ censortext.js
@@ -0,0 +1,29 @@
+/**
+ * Created by jingtingchao on 2018/7/16.
+ */
+
+var censoredWords = ["sad", "bad", "mad"];
+var customCensoredWords = [];
+function censor(inStr) {
+    for (idx in censoredWords) {
+        inStr = inStr.replace(censoredWords[idx], "****");
+    }
+
+    for (idx in customCensoredWords) {
+        inStr = inStr.replace(customCensoredWords[idx], "****");
+    }
+
+    return inStr;
+}
+
+function addCensoredWord(word) {
+    customCensoredWords.push(word);
+}
+
+function getCensoredWords() {
+    return censoredWords.contact(customCensoredWords);
+}
+
+exports.censor = censor;
+exports.addCensoredWord = addCensoredWord;
+exports.getCensoredWords = getCensoredWords;
